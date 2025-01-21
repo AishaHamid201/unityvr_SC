e961f0c0d886dcfc687e3a6a27b8e2c3ce5266ce
Shivam Chitnis <58934863+shivChitinous@users.noreply.github.com>
2025-01-21 11:12:04 -0500

    Update posAnalysis.py
    Rederive `dx_ft` if not in dataframe. Brings back egocentric values if not present. Fix vR, vT filtering.

--- unityvr/analysis/posAnalysis.py
+++ unityvr/analysis/posAnalysis.py
@@ -14,45 +14,19 @@ from os.path import sep, exists, join
 ##functions to process posDf dataframe
 
 #obtain the position dataframe with derived quantities
-def position(uvrDat, derive = True, rotate_by = None, filter_date = '2021-09-08', plot = False, plotsave=False, saveDir=None, computeVel=False, **computeVelocitiesKwargs):
+def position(uvrDat, derive = True, rotate_by = None, plot = False, plotsave=False, saveDir=None, computeVel=False, **computeVelocitiesKwargs):
     ## input arguments
     # set derive = True if you want to compute derived quantities (ds, s, dTh (change in angle), radangle (angle in radians(-pi,pi)))
     # rotate_by: angle (degrees) by which to rotate the trajectory to ensure the bright part of the panorama is at 180 degree heading.
-    # filter_date: date of experiment after which right handed angle convention will not be forced when loading posDf; this is because
-    #              converting from Unity's left handed angle convention to right handed convention was implemented after a certain 
-    #              date in the preproc.py file
     
-    posDf = uvrDat.posDf
-
-    #angle correction
-    #this is required only for data that was preprocessed before the filter_date
-    if (np.datetime64(uvrDat.metadata['date'])<=np.datetime64(filter_date)) & ('angle_convention' not in uvrDat.metadata):
-        print('correcting for Unity angle convention.')
-        posDf['angle'] = (-posDf['angle'])%360
-        uvrDat.metadata['angle_convention'] = "right-handed"
+    posDf = uvrDat.posDf.copy() #NOT INPLACE
 
     #rotate
     if rotate_by is not None:
-        #derive forward and side velocities: does not depend on rotation of theta
-        if 'dx' not in posDf:
-            xy = np.diff(posDf[['x', 'y']]/uvrDat.metadata['ballRad'], axis=0)
-            rotation_mats = np.array([np.array([[np.cos(theta), -np.sin(theta)],
-                                [np.sin(theta),  np.cos(theta)]]).T for theta in np.deg2rad(posDf['angle'])[:-1]])
-            posDf['dx'], posDf['dy'] = np.vstack([[0,0],np.einsum('ijk,ik->ij', rotation_mats, xy)]).T
-
+        
         #rotate the trajectory
         posDf['x'], posDf['y'] = rotation_deg(posDf['x'],posDf['y'],rotate_by)
-        
-        '''
-        INCORRECT CODE BLOCK, treats forward and side velocities like gradient of x and y
-        if 'dx' in posDf:
-            posDf['dx'], posDf['dy'] = rotation_deg(posDf['dx'],posDf['dy'],rotate_by)
-        else:
-            posDf['dx'] = np.gradient(posDf['x'])
-            posDf['dy'] = np.gradient(posDf['y'])
-        if 'dxattempt' in posDf:
-            posDf['dxattempt'], posDf['dyattempt'] = rotation_deg(posDf['dxattempt'],posDf['dyattempt'],rotate_by)
-        '''
+
         posDf['angle'] = (posDf['angle']+rotate_by)%360
         uvrDat.metadata['rotated_by'] = (uvrDat.metadata['rotated_by']+rotate_by)%360 if ('rotated_by' in uvrDat.metadata) else (rotate_by%360)
 
@@ -83,8 +57,15 @@ def posDerive(posDf):
     posDf['s'] = np.cumsum(posDf['ds']) #integrated pathlength from start
     posDf['dTh'] = (np.diff(posDf['angle'],prepend=posDf['angle'].iloc[0]) + 180)%360 - 180
     posDf['radangle'] = ((posDf['angle']+180)%360-180)*np.pi/180
-    posDf['Delx'] = np.gradient(posDf['x']) #allocentric translation vector x component
-    posDf['Dely'] = np.gradient(posDf['y']) #allocentric translation vector y component
+    posDf['dx'] = np.diff(posDf['x']) #allocentric translation vector x component
+    posDf['dy'] = np.diff(posDf['y']) #allocentric translation vector y component
+
+    #derive forward and side velocities: does not depend on rotation of trajectory and angle
+    if 'dx_ft' not in posDf:
+            xy = np.diff(posDf[['x', 'y']], axis=0)
+            rotation_mats = np.array([np.array([[np.cos(theta), -np.sin(theta)],
+                                [np.sin(theta),  np.cos(theta)]]).T for theta in np.deg2rad(posDf['angle'])[:-1]])
+            posDf['dx_ft'], posDf['dy_ft'] = np.vstack([[0,0],np.einsum('ijk,ik->ij', rotation_mats, xy)]).T #in egocentric frame, units: decimeters, #dx_ft is forward motion (forward is positive), dy_ft is side motion (rightward is positive)
     return posDf
 
 #segment flight bouts
@@ -151,19 +132,16 @@ def rotation_deg(x,y,theta):
 
 
 # Add derrived quantities: Velocities
-def computeVelocities(posDf, convf = 10, window=7, order=3):
-    #convf: conversion from dc (to cm if 10)
+def computeVelocities(inDf, window=7, order=3):
+    posDf = inDf.copy()
     #window and order for filter
-    from scipy.signal import savgol_filter
 
     # add derrived parameter to positional dataframe
-    posDf['vT'] = np.hypot(np.gradient(posDf.x.values*convf), np.gradient(posDf.y.values*convf))*(1/posDf.dt)
-    posDf['vR'] = np.gradient(np.unwrap(posDf.angle.values))
+    posDf['vT'] = np.hypot(np.gradient(posDf.x.values), np.gradient(posDf.y.values))*(1/posDf.dt) #translational velocity in decimeter/second
+    posDf['vR'] = np.gradient(np.unwrap(posDf.angle.values)) #rotational velocity in degrees/second
 
-    #TODO: REVISIT FILTERING AFTER INTERPOLATION
-
-    #posDf['vT_filt'] = savgol_filter(posDf.vT, window, order)
-    #posDf['vR_filt'] = savgol_filter(posDf.vR, window, order)
+    posDf['vT_filt'] = savgolFilterInterpolated(posDf, 'vT', window, order) #savgol filtered velocities
+    posDf['vR_filt'] = savgolFilterInterpolated(posDf, 'vR', window, order)
     return posDf
 
 def getTimeDf(uvrDat, trialDir, posDf = None, imaging = False, rate = 9.5509):
@@ -194,3 +172,20 @@ def getTimeDf(uvrDat, trialDir, posDf = None, imaging = False, rate = 9.5509):
     timeDf = carryAttrs(timeDf,posDf)
     
     return timeDf
+
+def savgolFilterInterpolated(posDf, value, window, order, timeStr = 'time', kind='linear'):
+        from scipy.signal import savgol_filter
+
+        df = posDf.copy()
+
+        #filtering with a savgol filter assumes that the data is evenly spaced
+        #interpolating the data to be evenly spaced
+        interpolated = sp.interpolate.interp1d(df[timeStr], df[value], kind=kind, bounds_error=False, fill_value='extrapolate')
+
+        equalTime = np.linspace(df[timeStr].min(),df[timeStr].max(),len(df[timeStr]))
+
+        filt = savgol_filter(interpolated(equalTime), window, order)
+
+        filtInterpolated = sp.interpolate.interp1d(equalTime, interpolated, kind=kind, bounds_error=False, fill_value='extrapolate')
+
+        return filtInterpolated(df.time)
